28afbec6224e7b0081ae010162e807a61094e485
Hunter David <33354098+mhunterdavid@users.noreply.github.com>
2018-07-02 21:50:08 -0400

    Began Strings.c

--- Strings.c
+++ Strings.c
@@ -3,4 +3,14 @@
 
 int main()
 {
+	char * first_name = "John"; //This is how to define a string in C through pointers
+	char[] last_name = "Doe";
+
+/* Basic string commands for code:
+'strlen' - A command used to return the length of a string which is passed as an argument
+'strncmp' - A command used to compare two strings, returning 0 if they equal, or not 0 if they don't equal
+'strcmp' - An unsafe version of strncmp
+*/
+	
+	 	
 	
